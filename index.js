63f75141ef9637ce73984a64511cda733c55690a
yugma26 <66039476+yugma26@users.noreply.github.com>
2022-09-26 23:26:39 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,89 @@ const inventory = newInventory()
 move(inventory).to(0, 0)
 
 const character = newImage('assets/green-character/static.gif')
+function handledirectionchange(direction){
+if(direction === null){
+    character.src = 'assets/green-character/static.gif'
+}
+if(direction === 'west'){
+    character.src = 'assets/green-character/west.gif'
+}
+if(direction === 'north'){
+    character.src = 'assets/green-character/north.gif'
+}
+if(direction === 'east'){
+    character.src = 'assets/green-character/east.gif'
+}
+if(direction === 'south'){
+    character.src = 'assets/green-character/south.gif'
+}
+}
+
+move(character).withArrowKeys(100, 250,handledirectionchange)
+
+
+let direction = null;
+let x = 100;
+let y = 250;
+
+function movecharacter(){
+     if (direction === 'west'){
+         x = x-1
+     }
+
+     if (direction === 'north'){
+         y = y+1
+       }
+
+       if (direction === 'east'){
+           x = x+1
+       }
+
+     if (direction === 'south'){
+          y = y-1
+      }
+   character.style.left = x +'px'
+   character.style.bottom = y +'px'
+
+}
 move(character).to(100, 250)
+setInterval(moveCharacter(), 1)
+setInterval(function(){ 
+    if(direction === 'west'){
+        x = x - 1
+    }
+    if(direction === 'north'){
+        y = y + 1
+    }
+    if(direction === 'east'){
+        x = x + 1
+    }
+    if(direction === 'south'){
+        y = y - 1
+    }
+    character.style.left = x + 'px'
+    character.style.bottom = y + 'px'
+}, 1)
+document.addEventListener('keydown', function(e){ 
+    if (e.repeat) return;             
+    if(e.key === 'ArrowLeft'){
+        direction = 'west'
+    }
+    if(e.key === 'ArrowUp'){
+        direction = 'north'
+    }
+    if(e.key === 'ArrowRight'){
+        direction = 'east'
+    }
+    if(e.key === 'ArrowDown'){
+        direction = 'south'
+    }
+})
+document.addEventListener('keyup', function(e){
+    direction = null
+})
+
+
 
 
 move(newImage('assets/tree.png')).to(200, 450)
